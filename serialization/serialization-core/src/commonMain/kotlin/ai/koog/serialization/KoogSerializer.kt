a47844d2bebfd0fff1bcc02fd6fd01aabbc124f2
Andrey Bragin <andrey.bragin@jetbrains.com>
2026-02-11 15:12:15 +0100

    Update KoogSerializer interface

--- serialization/serialization-core/src/commonMain/kotlin/ai/koog/serialization/KoogSerializer.kt
+++ serialization/serialization-core/src/commonMain/kotlin/ai/koog/serialization/KoogSerializer.kt
@@ -1,23 +1,42 @@
 package ai.koog.serialization
 
 /**
- * Serializer for converting objects to and from JSON.
+ * Serializer for converting values to and from JSON.
  */
 public interface KoogSerializer {
     /**
      * Serializes a value to its JSON representation.
      *
-     * @param value the object to serialize
-     * @return JSON string representation of the serialized value
+     * @param value value to serialize
+     * @param typeToken token capturing type information of [T]
+     * @return JSON string representation of the value
      */
-    public fun <T> serialize(value: T, token: TypeToken): String
+    public fun <T> serialize(value: T, typeToken: TypeToken): String
 
     /**
-     * Deserializes a JSON string back to an object.
+     * Deserializes a JSON string to a value of type [T].
      *
-     * @param value the JSON string to deserialize
-     * @param token type token describing the target type
-     * @return the deserialized object of type [T]
+     * @param value JSON string to deserialize
+     * @param typeToken token capturing type information of [T]
+     * @return deserialized value of type [T]
      */
-    public fun <T> deserialize(value: String, token: TypeToken): T
+    public fun <T> deserialize(value: String, typeToken: TypeToken): T
+
+    /**
+     * Serializes a value to its [JSONElement] representation.
+     *
+     * @param value value to serialize
+     * @param typeToken token capturing type information of [T]
+     * @return [JSONElement] representation of the value
+     */
+    public fun <T> serializeToJSONElement(value: T, typeToken: TypeToken): JSONElement
+
+    /**
+     * Deserializes [JSONElement] to a value of type [T].
+     *
+     * @param value [JSONElement] to deserialize.
+     * @param typeToken token capturing type information of [T]
+     * @return deserialized value of type [T]
+     */
+    public fun <T> deserializeFromJSONElement(value: JSONElement, typeToken: TypeToken): T
 }
